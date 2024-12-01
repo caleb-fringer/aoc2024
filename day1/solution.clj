2c1c7ccef415fbb93a5fa110ab49b8dd1fb41c0c
CalebF98 <cfringer98@gmail.com>
2024-12-01 13:37:40 -0800

    Refactored to a more idiomatic implementation.

--- day1/solution.clj
+++ day1/solution.clj
@@ -1,23 +1,56 @@
 (ns aoc2024.day1.solution
   (:require [clojure.java.io]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import (java.io File)))
 
-(def left (atom []))
-(def right (atom []))
+; I need to:
+; 1) Read the file
+; 2) line by line
+; 3) Split each line
+; 4) Cast each split number to an integer
+; 5) Accumulate each pair of integers into a map where we:
+;   a) Add the first number to left list
+;   b) Add the second number to right list
 
-(with-open [rdr (clojure.java.io/reader "input.txt")]
-  (doseq [line (line-seq rdr)
-          :when (not (str/blank? line))
-          :let [split-str (str/split line #"\s+")
-                l-acc nil
-                r-acc nil
-                l (Integer/parseInt (first split-str))
-                r (Integer/parseInt (last split-str))]]
-    (swap! left conj l)
-    (swap! right conj r)))
+; First, I am choosing to use java.io.Reader instead of slurp because
+; the input.txt file is large and I don't want to save it all in memory
+(defn not-blank? [s]
+  (not (str/blank? s)))
 
-(swap! left sort)
-(swap! right sort)
+; Reducing function
+(defn collect [acc [l r]]
+  ; We need to use a threading macro to ensure that the right update is
+  ; performed on the result of the left update
+  (-> acc
+      (update :left conj l)
+      (update :right conj r)))
 
-(reduce + (map #(abs (- %1 %2)) @left @right))
+(defn read-input [file]
+  (when (.exists (File. file))
+    (with-open [rdr (clojure.java.io/reader file)]
+      (->>
+       ; This is the initial value ->> is threading to the next fxns
+       (line-seq rdr)
+       (filter not-blank?)
+       (map #(str/split % #"\s+"))
+       ; Remember: map is being applied to the sequence supplied by ->>,
+       ; NOT each vector element. Need to nest/destructure each vector
+       ; element to perform parse-long on each string and keep them
+       ; grouped together
+       (map #(mapv parse-long %))
+       ; Now, reduce it into a map. This forces evaluation, whereas take
+       ; is never evaluated and closes the stream.
+       (reduce collect {:left [] :right []})))))
 
+(def input (read-input "input.txt"))
+
+; Calculate similarity for part one
+(defn similarity-score-one [[l r]]
+  (abs (- l r)))
+
+(defn part-one [m]
+  (let [l (sort (get m :left))
+        r (sort (get m :right))]
+    (reduce + (map similarity-score-one (map vector l r)))))
+
+(part-one input)

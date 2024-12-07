1fe6e269c9bb0abeadf7d57933bbb8a0253c9371
CalebF98 <cfringer98@gmail.com>
2024-12-06 21:45:50 -0800

    Solved day 4 pt 2.

--- day4/solution.clj
+++ day4/solution.clj
@@ -20,6 +20,8 @@
 (defn != [a b]
   (not (== a b)))
 
+(def test-input (read-input "test.txt"))
+
 (defn in-bounds? [coords dimensions]
   (every? true? (into (map #(>= % 0) coords)
                       (map #(< %1 %2) coords dimensions))))
@@ -58,3 +60,38 @@
               (reduce + (map #(check-xmas [row col] % grid) (generate-directions)))))))
 
 (part-one input)
+
+(defn check-mas [pos dir grid]
+  (let [dimensions [(count grid)
+                    (count (first grid))]]
+    (->>
+     (map #(vector (add-vector pos (scale-vector %1 dir)) %2) (range -1 2)
+          ["M" "A" "S"])
+     (every? #(and (in-bounds? (first %) dimensions)
+                   (= (get-in grid (first %)) (second %)))))))
+
+(check-mas [1 2] [1 1] test-input)
+
+(defn x-mas? [pos [a b] grid]
+  (and
+   (or
+    (check-mas pos a grid)
+    (check-mas pos (scale-vector -1 a) grid))
+   (or
+    (check-mas pos b grid)
+    (check-mas pos (scale-vector -1 b) grid))))
+
+(defn check-x-mas [pos grid]
+  (let [valid-pairs '([[1 1] [1 -1]])]
+    (->>
+     (mapv #(x-mas? pos % grid) valid-pairs)
+     (mapv {false 0 true 1}))))
+
+(defn part-two [grid]
+  (let [n-rows (count grid)
+        n-cols (count (first grid))]
+    (reduce + (for [col (range n-cols)
+                    row (range n-rows)]
+                (reduce + (check-x-mas [row col] grid))))))
+
+(part-two input)
